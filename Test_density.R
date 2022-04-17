1d6a3005827c9b16b2d6a4a71b6b6bae9c98551d
ribeiropvb <65306388+ribeiropvb@users.noreply.github.com>
2022-04-17 11:36:09 -0300

    Add files via upload

--- Test_density.R
+++ Test_density.R
@@ -3,9 +3,10 @@ library(tidyverse)
 library(magrittr)
 library(fitdistrplus)
 
-x <- rexp(500, 0.1)
+x <- data("GasolineYield")
+x <- GasolineYield$yield
 
-if(x == round(x,0)){
+if(sum(x == round(x,0)) == length(x)){
   
   n <- trunc(binomial_estimation(x))
   
@@ -44,31 +45,38 @@ if(x == round(x,0)){
   
   if(mod_selec == 'norm'){
     
+    source('https://raw.githubusercontent.com/ribeiropvb/density_test/main/discrete/discrete_normal.R')
     discrete_normal(x)
     
   } else if(mod_selec == 'logis'){
     
+    source('https://raw.githubusercontent.com/ribeiropvb/density_test/main/discrete/discrete_logis.R')
     discrete_logis(x)
     
   } else if(mod_selec == 'binom'){
     
+    source('https://raw.githubusercontent.com/ribeiropvb/density_test/main/discrete/Binominal_Estimation/binomial_estimation.R')
+    source('https://raw.githubusercontent.com/ribeiropvb/density_test/main/discrete/discrete_binom.R')
     discrete_binom(x)
     
   } else if(mod_selec == 'nbinom'){
     
+    source('https://raw.githubusercontent.com/ribeiropvb/density_test/main/discrete/discrete_nbinom.R')
     discrete_nbinom(x)
     
   } else if(mod_selec == 'geom'){
     
+    source('https://raw.githubusercontent.com/ribeiropvb/density_test/main/discrete/discrete_geom.R')
     discrete_geom(x)
     
   } else if(mod_selec == 'pois'){
     
+    source('https://raw.githubusercontent.com/ribeiropvb/density_test/main/discrete/discrete_pois.R')
     discrete_pois(x)
     
   }
   
-} else{
+} else {
   
   fit.norm <- fitdist(x, dist = 'norm')
   fit.lnorm <- fitdist(x, dist = 'lnorm')
@@ -109,37 +117,74 @@ if(x == round(x,0)){
     )
   }
   
+  resumo %<>%
+    arrange(BIC)
   mod_selec <- resumo %>%
-    arrange(BIC) %>%
     .$distr %>% .[1]
   err <- try(fitdist(x, mod_selec)) %>% class
   beta_test <- sum(between(x, 0, 1)) == length(x)
   
   if(mod_selec == 'norm'){
     
-    cont_normal(x)
+    source('https://raw.githubusercontent.com/ribeiropvb/density_test/main/continuous/cont_norm.R')
+    fit <- cont_normal(x)
     
   } else if(mod_selec == 'lnorm'){
     
-    cont_exp(x)
+    source('https://raw.githubusercontent.com/ribeiropvb/density_test/main/continuous/cont_lnormal.R')
+    fit <- cont_lnormal(x)
     
   }  else if(err != 'try-error' & mod_selec == 'exp'){
     
-    cont_exp(x)
+    source('https://raw.githubusercontent.com/ribeiropvb/density_test/main/continuous/cont_exp.R')
+    fit <- cont_exp(x)
     
   } else if(err != 'try-error' & mod_selec == 'gamma'){
     
-    cont_gamma(x)
+    source('https://raw.githubusercontent.com/ribeiropvb/density_test/main/continuous/cont_gamma.R')
+    fit <- cont_gamma(x)
     
   }  else if(err != 'try-error' & mod_selec == 'weibull'){
     
-    cont_weibull(x)
+    source('https://raw.githubusercontent.com/ribeiropvb/density_test/main/continuous/cont_weibull.R')
+    fit <- cont_weibull(x)
     
   }  else if(beta_test & mod_selec == 'beta'){
     
-    cont_beta(x)
+    source('https://raw.githubusercontent.com/ribeiropvb/density_test/main/continuous/cont_beta.R')
+    fit <- cont_beta(x)
     
   }
   
 }
 
+attach(fit)
+Modelo <- resumo[1,1]
+theme_clear <- theme_light() +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank())
+
+print(fit.model)
+fit.model %>% plot()
+
+plot(
+  ecdf(stats), las = 1, lwd = 1.7
+  , main = paste("KS-test statistic simulation (CDF) -", Modelo)
+  , col = "steelblue"
+)
+
+#source('https://raw.githubusercontent.com/ribeiropvb/density_test/main/plot_function/generate_plot.R')
+
+#-----------------------------------------------------------------------------
+# Plot PDF
+#-----------------------------------------------------------------------------
+
+generate_plot(data = boot.pdf, contexto = 'pdf')
+
+#-----------------------------------------------------------------------------
+# Plot CDF
+#-----------------------------------------------------------------------------
+
+generate_plot(data = boot.cdf, contexto = 'cdf')
